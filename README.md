a4567ccbf632e18d992fefd0bfb7e34bf29a7d15
Админ <KorostelevDima@yandex.ru>
2022-09-25 20:21:07 +0300

    New commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Python 3.10.5
+
+To launch the application:
+
+1) Download and go to the downloaded directory in the console
+2) Enter "pip install -r requirements.txt"
+3) Enter "python manage.py runserver"
+4) Open the page in a browser using the link http://127.0.0.1:8000/
+5) And you will see Main page with different tasks. You can read tasks, 
+delete tasks, or go to the "Create task" tab at the top right
+6) In the "Create task" tab you can enter title of the new task, description of
+the new task and add this task to the Main page
+7) You can also login to the admin panel using link: http://127.0.0.1:8000/admin (login:admin, password: 12345).
+Through it, you can manage user authorization, see recent actions, as well as delete and add tasks
---
+++ main/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from .models import Task
+from .forms import TaskForm
+
+
+def index(request):
+    tasks = Task.objects.order_by('-id')
+    return render(request, 'main/index.html', {'title': 'Site main page', 'tasks': tasks})
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = 'Incorrect data'
+
+    form = TaskForm()
+    context = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'main/create.html', context)
+
+
+def delete(request, id):
+    if request.method == 'GET':
+        task = Task.objects
+        task.get(id=id).delete()
+
+    return redirect('home')
